68ac2e80175a392df608b2a4c0315ab96f7e3d41
MelihCelik00 <melihsafa.c@gmail.com>
2022-12-24 10:44:41 +0300

    added first homework files, but not finished

--- hw1/index.js
+++ hw1/index.js
@@ -6,5 +6,15 @@ fetch(url)
     .then(function(data) {
         document.getElementById('content').innerHTML = JSON.stringify(data);
     });
-}
+};
 
+console.log(loadJSON(fetchResult));
+
+const listDiv = document.createElement("div");
+listDiv.className = "box";
+listDiv.id = list;
+let container = document.getElementById('containerId');
+
+function create() {
+    container.appendChild(list)
+}
